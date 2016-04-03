494a4a86982df59dc57044d92a73581dd76a87ff
Renato Andrade <renatoandradej@gmail.com>
2016-04-03 01:57:02 -0400

    "Create Business" Activity Created
    - layout
    - icons for "done" and "cancel" added
    - menu_create created

--- TamoJunto/app/src/main/java/com/renatoandrade/tamojunto/CreateBusiness.java
+++ TamoJunto/app/src/main/java/com/renatoandrade/tamojunto/CreateBusiness.java
@@ -1,50 +1,38 @@
 package com.renatoandrade.tamojunto;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.util.Log;
-import android.view.View;
+import android.telephony.PhoneNumberFormattingTextWatcher;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.Button;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.Spinner;
 
-public class MainActivity extends AppCompatActivity {
+public class CreateBusiness extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_create_business);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        Spinner spinner = (Spinner) findViewById(R.id.spnType);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.business_categories, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
 
-        Button btnCreate = (Button) findViewById(R.id.btnCreate);
-
-        btnCreate.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent i = new Intent(getApplicationContext(), CreateBusiness.class);
-                startActivity(i);
-            }
-        });
+        EditText txtPhone = (EditText) findViewById(R.id.txtPhone);
+        txtPhone.addTextChangedListener(new PhoneNumberFormattingTextWatcher());
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
+        getMenuInflater().inflate(R.menu.menu_create, menu);
         return true;
     }
 
@@ -56,10 +44,18 @@ public class MainActivity extends AppCompatActivity {
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
+        if (id == R.id.action_cancel) {
+            finish();
+            return true;
+        }
+        if (id == R.id.action_done) {
+            return true;
+        }
         if (id == R.id.action_settings) {
             return true;
         }
 
         return super.onOptionsItemSelected(item);
     }
+
 }
